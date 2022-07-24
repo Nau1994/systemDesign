8a8875c69cb27eff811707bb2438de255ddc6134
Naushad <naushad.a1013@gmail.com>
2022-07-24 21:37:18 +0530

    hello

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    cout<<n+1;
+    cout<<endl;
+}
